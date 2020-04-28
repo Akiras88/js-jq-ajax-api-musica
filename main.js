e7264cee77dbd272fbbee012d1b4731f92966070
Francesco <francesco.scarnera3@gmail.com>
2020-04-28 17:43:19 +0200

    append albums for length of API

--- main.js
+++ main.js
@@ -12,27 +12,30 @@
 $(document).ready(function() {
 	
 	var myApi = 'https://flynn.boolean.careers/exercises/api/array/music';
-	var container = $('.container');
-
+	var container = $('.cds-container');
 	// init template
 	var source = $("#entry-template").html();
 	var template = Handlebars.compile(source);
+
 	// call api
 	$.ajax({
 		url: myApi,
 		method: 'GET',
 		success: function(data) {
-			var albumApi = data.response[0];
-			var albums = {
-				poster : albumApi.poster,
-				title : albumApi.title,
-				author : albumApi.author,
-				year : albumApi.year
+			var albumApi = data.response;
+			for ( var i = 0; i < albumApi.length; i++ ) {
+				var cd = albumApi[i];
+				var albums = {
+					poster : cd.poster,
+					title : cd.title,
+					author : cd.author,
+					year : cd.year
+				}
+				console.log(albums);
+				// add template
+				var html = template(albums);
+				container.append(html);
 			}
-			console.log(albums);
-			// add template
-			var html = template(albums);
-			container.append(html);
 		},
 		error: function() {
 			console.log('Error');
